40458839e4683fcec6aee28c952dea824ebbf68e
RandomAdversary <RandomAdversary@gmail.com>
2018-03-31 22:03:15 +0200

    Minor improvements

--- src/main/java/PwnedPasswords.java
+++ src/main/java/PwnedPasswords.java
@@ -1,6 +1,8 @@
 import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -9,9 +11,9 @@ import java.security.NoSuchAlgorithmException;
 public class PwnedPasswords {
 
     private static String userAgent = "PwnedPasswords by RandomAdversary | github.com/RandomAdversary/PwnedPasswords";
-    private static String apiVersion = "V2";
-    private static String apiBaseURL = "https://haveibeenpwned.com/api/";
+    private static String apiBaseURL = "https://api.pwnedpasswords.com";
     private static String apiPwnedPasswordURL = "/pwnedpassword/";
+    private static String apiRangeURL = "/range/";
 
     /**
      * Checks for hacked passwords using have I been pwned API.
@@ -49,7 +51,10 @@ public class PwnedPasswords {
      * @return true if the password was found in the Pwned Passwords repository, false if  the password was not found in the Pwned Passwords repository.
      */
     public static boolean hashAndPwn(String password) throws BadRequestException, NoSuchAlgorithmException, IOException, RateLimitException, UnknownResponseCode {
+        return pwnPassword(hashPassword(password));
+    }
 
+    private static String hashPassword(String password) throws BadRequestException, NoSuchAlgorithmException, UnsupportedEncodingException {
         if (password.length() == 0)
             throw new BadRequestException("Bad request — the password does not comply with an acceptable format.");
 
@@ -61,21 +66,53 @@ public class PwnedPasswords {
         for (int i = 0; i < result.length; i++) {
             sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
         }
-        return pwnPassword(sb.toString());
+        return sb.toString();
     }
 
+ /*   public static boolean pwnByRange(String password) throws NoSuchAlgorithmException, BadRequestException, IOException, UnknownResponseCode, RateLimitException {
+        String hashedPassword = hashPassword(password).toUpperCase();
+        String range = hashedPassword.substring(0, 5).toUpperCase();
+        //Note: The api is case insensitive.
+
+        String URL = apiBaseURL + apiRangeURL + range;
+        //Get request to get the range
+        URL obj = new URL(URL);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", userAgent);
+        int responseCode = con.getResponseCode();
+        //Validate the response code
+        if (responseCode == 429)
+            throw new RateLimitException("Too many requests — the rate limit has been exceeded.\nLearn more at https://haveibeenpwned.com/API/v2#RateLimiting");
+
+        if (responseCode != 200) {
+            throw new UnknownResponseCode("Sorry, something went wrong.\nHttp status code of the response: " + responseCode);
+        }
+        //If everything is ok, read the data
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        String data = response.toString().toUpperCase();
+        return data.contains(hashedPassword);
+    }*/
 
     private static int requestPwnStatus(String password) throws IOException {
-        String URL = apiBaseURL + apiVersion + apiPwnedPasswordURL + password;
-        return makeGetRequest(URL);
+        String URL = apiBaseURL + apiPwnedPasswordURL + password;
+        return GET(URL);
     }
 
     // Code shamelessly copied from https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
-    private static int makeGetRequest(String URL) throws IOException {
+    private static int GET(String URL) throws IOException {
         URL obj = new URL(URL);
         HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("User-Agent", userAgent);
         return con.getResponseCode();
     }
+
 }
